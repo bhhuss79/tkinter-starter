e6bd61315ba441061c716c8b8a97052287e20b89
Bahaa Hussein <bhhuss79>
2020-01-28 11:08:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
 # Add a label with the text "Hello"
-lbl = Label(window, text="Hello")
+lbl = Label(window, text="Bahaa Hussein")
 
 # Place the label in the window at 0, 0
-lbl.grid(column=0, row=0)
+lbl.grid(column=5000, row=0)
 
 
 window.mainloop()     # Keep the window open
